4bf04cf8bac7f0e5e70188e51a281136bbcf99fd
gahyeon <1000ji1@naver.com>
2023-05-03 22:21:58 +0900

    부트스트랩 적용

--- VueDjangoProject/mysite/settings.py
+++ VueDjangoProject/mysite/settings.py
@@ -121,6 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICIFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
